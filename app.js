a25eb1cc6a74ad3cd81f11c27b65a72df1faeae1
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-18 13:14:51 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//Es.1 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+//Es.2 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
